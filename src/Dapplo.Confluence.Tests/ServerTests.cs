91f784e4a175dd097936605cb08e7a64295bb0cd
Robin Krom <robin@getgreenshot.org>
2021-10-27 22:18:33 +0200

    Added a test case, one of multiple tries to see if I could reproduce #53 [skip ci]

---
+++ src/Dapplo.Confluence.Tests/ServerTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Dapplo and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dapplo.Confluence.Tests
+{
+    /// <summary>
+    ///     Tests
+    /// </summary>
+    [CollectionDefinition("Dapplo.Confluence")]
+    public class ServerTests : ConfluenceIntegrationTests
+    {
+        public ServerTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public async Task Test_IsCloud()
+        {
+            var isCloudServer = await ConfluenceTestClient.IsCloudServer();
+            Assert.True(isCloudServer);
+        }
+    }
+}
